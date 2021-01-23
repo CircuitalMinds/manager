79ca596b95353f4caf71c2d105524e614c9eb92e
Alan Matzumiya <alan.matzumiya@outlook.es>
2021-01-22 23:41:00 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,9 @@
-from flask import Flask, request, flash, url_for, redirect, render_template, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import request, flash, url_for, redirect, render_template, jsonify
 import yaml
 import requests
+from database import container_data, app, db
 
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///container_data.sqlite3'
-app.config['SECRET_KEY'] = "circuitalminds"
-
-db = SQLAlchemy(app)
-
-class container_data(db.Model):
-    id = db.Column('file_id', db.Integer, primary_key = True)
-    container = db.Column(db.String(100))
-    name = db.Column(db.String(100))
-    url = db.Column(db.String(100))
-
-    def __init__(self, container, name, url):
-       
-       self.container = container
-       self.name = name
-       self.url = url
-
-    def __repr__(self):
-    
-        return '<container_data %r>' % self.name
-    
 @app.route('/')
 def show_data():
     
