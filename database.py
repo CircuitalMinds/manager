d9f1f47152a156d17e7ad66061ff6e3e6c82e5ff
Alan Matzumiya <alan.matzumiya@outlook.es>
2021-01-22 23:37:38 -0700

    Create database.py

---
+++ database.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///container_data.sqlite3'
+app.config['SECRET_KEY'] = "circuitalminds"
+
+db = SQLAlchemy(app)
+
+class container_data(db.Model):
+    id = db.Column('file_id', db.Integer, primary_key = True)
+    container = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+    url = db.Column(db.String(100))
+    
+    def __init__(self, container, name, url):
+       
+       self.container = container
+       self.name = name
+       self.url = url
+           
+    def __repr__(self):
+    
+        return '<container_data %r>' % self.name 
