99bae165dcaab75144262213d8f65af4c27c19f0
Alan Matzumiya <alan.matzumiya@outlook.es>
2021-01-26 02:25:40 -0700

    Update circuitflow.py

--- circuitflow.py
+++ circuitflow.py
@@ -13,14 +13,22 @@ def get_data(book):
     if request.args.get('token') == circuitflow.config['SECRET_KEY']:
         file_name = request.args.get("name")
         fdata = {}
-        if file_name == None:
-            for file_data in books_data[book].query.all():
+        if file_name == "playlist":
+            data = books_data[book].query.filter(books_data[book].name.endswith(".mp4")).all()
+            for file_data in data:
+                fdata[file_data.id] = {}
+                for key in books_data[book].args["attrs"]:
+                    fdata[file_data.id][key] = file_data.__dict__[key]     
+            return jsonify(fdata)
+        
+        elif file_name == None:
+            data = books_data[book].query.all()
+            for file_data in data:
                 fdata[file_data.id] = {}
                 for key in books_data[book].args["attrs"]:
                     fdata[file_data.id][key] = file_data.__dict__[key]     
             return jsonify(fdata)
         else:
-
             file_data = books_data[book].query.filter(books_data[book].name == file_name).first_or_404(description='There is no data with {}'.format(file_name))
             fdata[file_data.id] = {}
             for key in books_data[book].args["attrs"]:
