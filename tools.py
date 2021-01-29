50db45eac353e2a7da00e458e6dabe9a54ab8985
Alan Matzumiya <alan.matzumiya@outlook.es>
2021-01-28 19:36:26 -0700

    Update tools.py

--- tools.py
+++ tools.py
@@ -5,9 +5,10 @@ import requests
 def data_files(path):
     
     dict_data = {}
-    data = yaml.load(requests.get(path).content, Loader=yaml.FullLoader)
-    for dirs in list(data.keys()):
-        for key in list(data[dirs].keys()):
-            dict_data[key] = data[dirs][key]
+    
+    for name in list(path.keys()):
+        data = yaml.load(requests.get(path[name]).content, Loader=yaml.FullLoader)
+        for key in list(data.keys()):
+            dict_data[key] = data[key]
             
     return dict_data
