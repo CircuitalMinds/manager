88ff58eb2a005be800fb3c92220355ecfe3fe8b1
Alan Matzumiya <alan.matzumiya@outlook.es>
2021-02-17 21:07:01 -0700

    Update circuitflow.py

--- circuitflow.py
+++ circuitflow.py
@@ -1,12 +1,107 @@
-from flask import request, jsonify
+from flask import request, jsonify, Flask
 import yaml
-from database import workers, music_data, notebooks_data, repos_data, circuitflow, db
+from flask_sqlalchemy import SQLAlchemy
 
-books_data = {"music": music_data, "notebooks": notebooks_data, "repositories": repos_data}
+
+circuitflow = Flask(__name__)
+circuitflow.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
+circuitflow.config['SECRET_KEY'] = "circuitalminds"
+
 _config = yaml.load(open('./manager/_config.yml'), Loader=yaml.FullLoader)
 HOST = _config["HOST"]
 PORT = _config["PORT"]
 DEBUG = _config["DEBUG"]
+
+db = SQLAlchemy(circuitflow)
+
+class music_data(db.Model):
+    id = db.Column('file_id', db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    url = db.Column(db.String(100))
+    args = {
+        "attrs": ["name", "url"]
+    }
+    
+    def __init__(self, data):       
+       self.name = data["name"]
+       self.url = data["url"]
+
+    def __repr__(self):
+    
+        return '<containers_data %r>' % self.name
+
+class notebooks_data(db.Model):
+    id = db.Column('notebook_id', db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    url = db.Column(db.String(100))
+    args = {
+        "attrs": ["name", "url"]
+    }
+    
+    def __init__(self, data):
+       self.name = data["name"]
+       self.url = data["url"]
+           
+    def __repr__(self):
+    
+        return '<notebooks_data %r>' % self.name        
+
+class repos_data(db.Model):
+    id = db.Column('repo_id', db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    url = db.Column(db.String(100))
+    args = {
+        "attrs": ["name", "url"]
+    }
+    
+    def __init__(self, data):
+       self.name = data["name"]
+       self.url = data["url"]
+           
+    def __repr__(self):
+    
+        return '<repos_data %r>' % self.name
+
+
+class users_data(db.Model):
+    id = db.Column('user_id', db.Integer, primary_key = True)
+    username = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    args = {
+        "attrs": ["username", "email"]
+    }
+    
+    def __init__(self, data):
+       self.username = data["username"]
+       self.email = data["email"]
+           
+    def __repr__(self):
+    
+        return '<users_data %r>' % self.username 
+
+
+class workers(db.Model):
+    id = db.Column('worker_id', db.Integer, primary_key = True)
+    worker = db.Column(db.String(100))
+    job = db.Column(db.String(100))
+    argument = db.Column(db.String(100))
+    status = db.Column(db.String(100))
+    args = {
+        "attrs": ["worker", "job", "argument", "status"]
+    }
+    
+    def __init__(self, worker, job, argument, status):
+       self.worker = worker
+       self.job = job
+       self.argument = argument
+       self.status = status
+           
+    def __repr__(self):
+    
+        return '<workers %r>' % self.worker
+
+    
+books_data = {"music": music_data, "notebooks": notebooks_data, "repositories": repos_data}
 
 
 @circuitflow.route('/workers_info')
